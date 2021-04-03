efb914f4a4fecae20b03b7fa2f1d9ba415c1a664
jorgeluisAG <jorge.luis.alejo.gutierrez@gmail.com>
2021-04-03 19:07:30 -0400

    Agregando codigo

--- src/main/java/bo/ucb/edu/ingsoft/bl/SellerBl.java
+++ src/main/java/bo/ucb/edu/ingsoft/bl/SellerBl.java
@@ -86,4 +86,8 @@ public class SellerBl {
         seller.setImagePath(nombre);
         sellerDao.updateImage(seller);
     }
+    public List<PublicationSimpleRequest> publications(Integer idSeller,Integer i,Integer n){
+        List<PublicationSimpleRequest> publications=sellerDao.publications(idSeller,i,n);
+        return publications;
+    }
 }
