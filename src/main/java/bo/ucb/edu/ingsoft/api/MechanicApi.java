42d8f019d8b4a15baac8d99bda225ada64e4f7f2
jorgeluisAG <jorge.luis.alejo.gutierrez@gmail.com>
2021-04-26 17:55:37 -0400

    Agregando las funciones del mecanico

--- src/main/java/bo/ucb/edu/ingsoft/api/MechanicApi.java
+++ src/main/java/bo/ucb/edu/ingsoft/api/MechanicApi.java
@@ -1,10 +1,31 @@
 package bo.ucb.edu.ingsoft.api;
 
+import bo.ucb.edu.ingsoft.bl.MechanicBl;
+import bo.ucb.edu.ingsoft.dto.MechanicSimpleRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/mechanic")
 public class MechanicApi {
+    private MechanicBl mechanicBl;
+
+    @Autowired
+    public MechanicApi(MechanicBl mechanicBl){
+        this.mechanicBl = mechanicBl;
+    }
+
+    @RequestMapping(method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MechanicSimpleRequest> mechanics(HttpServletRequest request) {
+        List<MechanicSimpleRequest> mechanic=mechanicBl.mechanics();
+        return mechanic;
+    }
+
 
 }
---
+++ src/main/java/bo/ucb/edu/ingsoft/bl/MechanicBl.java
@@ -0,0 +1,28 @@
+package bo.ucb.edu.ingsoft.bl;
+
+import bo.ucb.edu.ingsoft.dao.MechanicDao;
+import bo.ucb.edu.ingsoft.dao.TransactionDao;
+import bo.ucb.edu.ingsoft.dto.MechanicSimpleRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MechanicBl {
+    private MechanicDao mechanicDao;
+    private TransactionDao transactionDao;
+
+    @Autowired
+    public MechanicBl(MechanicDao mechanicDao, TransactionDao transactionDao){
+        this.mechanicDao = mechanicDao;
+        this.transactionDao = transactionDao;
+    }
+
+    public List<MechanicSimpleRequest> mechanics(){
+        List<MechanicSimpleRequest> mechanic=mechanicDao.mechanics();
+        return mechanic;
+    }
+
+
+}
