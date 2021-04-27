5079ea88e8bfd483e91244e41db58210470e6b89
Silvana <silvana.munoz.vargas@gmail.com>
2021-04-26 22:31:45 -0400

    Api para calificar

--- src/main/java/bo/ucb/edu/ingsoft/dao/MechanicDao.java
+++ src/main/java/bo/ucb/edu/ingsoft/dao/MechanicDao.java
@@ -1,6 +1,8 @@
 package bo.ucb.edu.ingsoft.dao;
 
 import bo.ucb.edu.ingsoft.dto.MechanicSimpleRequest;
+import bo.ucb.edu.ingsoft.dto.QualifyMechanicRequest;
+import bo.ucb.edu.ingsoft.model.Mechanic;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -8,4 +10,9 @@ import java.util.List;
 @Mapper
 public interface MechanicDao {
     public List<MechanicSimpleRequest> mechanics();
+    public void qualifyMechanic(QualifyMechanicRequest qualifyMechanicRequest);
+
+    public Mechanic stars (Integer idMechanic);
+
+    public void starsValue(Mechanic mechanic);
 }
