20fa805695419cecafc6280d8458799d82770434
Silvana <silvana.munoz.vargas@gmail.com>
2021-05-11 22:58:17 -0400

    Modificacion cors para web

--- src/main/java/bo/ucb/edu/ingsoft/api/LoginApi.java
+++ src/main/java/bo/ucb/edu/ingsoft/api/LoginApi.java
@@ -4,14 +4,10 @@ import bo.ucb.edu.ingsoft.bl.LoginBl;
 import bo.ucb.edu.ingsoft.dto.UserRequest;
 import bo.ucb.edu.ingsoft.model.Transaction;
 import bo.ucb.edu.ingsoft.util.TransactionUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
